74aaf1154b99e373d3831a212263296685baa0c5
Rinat Khisamytdinov <rinat.igorevich@yandex.ru>
2021-10-04 19:48:42 +0300

    тренируем гит

---
+++ view/View.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\View;
+
+class View implements Renderable
+{
+    /*
+     * Пример использования этого класса в контроллере:
+     * return new View('homepage', ['title' => 'Index Page']);
+     */
+    private string $view;
+
+    private array $data;
+
+    /**
+     * @param string $view
+     * @param array $data
+     */
+
+    public function __construct(string $view, array $data)
+    {
+        $this->view = $view;
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        extract($this->data);
+        include $this->getIncludeTemplate($this->view);
+        /*
+         * метод должен выводить необходимый шаблон.
+         * Внутри метода нужно распаковать данные свойства $data в переменные
+         * (для этого просто примените функцию extract())
+         *, а затем подключить страницу шаблона,
+         * получив полный путь к ней с помощью другого метода этого класса getIncludeTemplate().
+         */
+    }
+
+    private function getIncludeTemplate($view)
+    {
+
+        $template =
+        /*
+         * метод должен сформировать абсолютный путь к файлу шаблона,
+         * указанного в свойстве $view с использованием константы VIEW_DIR.
+         * При этом в качестве шаблона можно указать строку personal.messages.show,
+         * в этом случае метод должен подключить файл шаблона personal/messages/show.php,
+         * который должен располагаться в директории view.
+         */
+    }
+}
